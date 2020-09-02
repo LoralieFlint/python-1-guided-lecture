eeebbfc6156038d866cddc24596e83e8c8c248bb
ittyBittyBytes <loralie.flint0128@gmail.com>
2020-09-01 20:00:33 -0700

    format strings lists and dictionaries

--- python-1-guided.py
+++ python-1-guided.py
@@ -1,12 +1,94 @@
+# enumerate()
+
+# .items()
+
+
+
+
 # print to the console
 print("hello World!")
 
-for i in range(0, 11): # starts at 0 goes to 10
-    print(i)
+# for i in range(0, 11): # starts at 0 goes to 10
+#     print(i)
 
-for i in range(10, -1, -1): # starts at 10 counts down to 0
-    print(i)
+# for i in range(10, -1, -1): # starts at 10 counts down to 0
+#     print(i)
 
 # for i in range(1000): # counts all the way to 999 from 0
 #     print(i)
+
+# a = "Hello "
+# b = "World! "
+# c = 365
+# print(a + b + str(c)) # str turns number vairiable to a string
+
+# a = 15
+# b = 10
+# c = "365"
+# print(a + b + int(c)) # int turns string vairiable to an integer
+
+
+# lists
+a = [10, 20, 30, 40, 50]
+# for num in a:
+#     print(num)
+
+# for i in range(len(a)):
+#     print(a[i])
+
+# for i in range(len(a)):
+#     # print(str[i] + " :: " + str(a[i]))
+#     print(f"{i}: {a[i]}")
+
+
+# print(len(a))
+
+
+# for num in enumerate(a):
+#     print(num)
+
+# insert to front of list
+a.append(4444)
+print(a)
+
+# add to the end of a list
+# a.pop(365)
+# print(a)
+
+
+# insert to index of list
+a.insert(0, 999)
+print(a)
+
+# dictionary
+d = {
+    'Goats': 12,
+    'foo': 10,
+    'bar': 20,
+    'baz': 30
+}
+d['beej'] = 33
+
+# for i in d:
+#     print(f"{i}: {d[i]}")
+
+
+for k, v in d.items():
+    print(f"{k}: {v}")
+
+
+# list comprehension
+
+
+
+# dictionary comprehension
+
+
+
+
+
+
+
+
+
 
