1eaa3bb3f149bc47c2a3d594d9152b371a85f414
ittyBittyBytes <loralie.flint0128@gmail.com>
2020-09-01 19:18:21 -0700

    loops and prints

---
+++ python-1-guided.py
@@ -0,0 +1,12 @@
+# print to the console
+print("hello World!")
+
+for i in range(0, 11): # starts at 0 goes to 10
+    print(i)
+
+for i in range(10, -1, -1): # starts at 10 counts down to 0
+    print(i)
+
+# for i in range(1000): # counts all the way to 999 from 0
+#     print(i)
+
